1383f96985d0f6390160380511b0c14a445b40d2
BayhanR <91837307+BayhanR@users.noreply.github.com>
2024-05-19 02:36:00 +0300

    Add files via upload

---
+++ Ortalama Hesaplama/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    // Özyinelemeli Fibonacci fonksiyonu
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+            return n;
+        else
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+
+    static void Main()
+    {
+        Console.Write("Derinliği giriniz: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Fibonacci dizisi 0'dan {n}'e kadar:");
+        // Fibonacci dizisini oluştur ve elemanlarını topla
+        int toplam = 0;
+        for (int i = 0; i <= n-1; i++)
+        {
+            int fib = Fibonacci(i);
+            toplam += fib;
+            Console.WriteLine($"Fibonacci({i}) = {fib}");
+        }
+        // Dizinin ortalamasını hesapla
+        double ortalama = (double)toplam / (n);
+        // Ortalamayı yazdır
+        Console.WriteLine($"Fibonacci dizisinin ilk {n} elemanının ortalaması: {ortalama}");
+    }
+}
---
+++ Üçgen çizme/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UcgenCiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Üçgenin boyutunu belirtin:");
+            int boyut = Convert.ToInt32(Console.ReadLine());
+
+            Ucgen ucgen = new Ucgen();
+            ucgen.Ciz(boyut);
+        }
+    }
+
+    public class Ucgen
+    {
+        public void Ciz(int boyut)
+        {
+            for (int i = 0; i < boyut; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
